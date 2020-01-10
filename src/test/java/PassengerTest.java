5842667c43c23e05564c6d7d9e70d124a0d058ed
Morgaine <morgainedv@gmail.com>
2020-01-10 18:20:34 +0000

    adds passengerhasname method

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    private Passenger passenger1;
+    private Passenger passenger2;
+
+    @Before
+    public void before (){
+       passenger1 = new Passenger("Eugene", 1);
+       passenger2 = new Passenger(  "Juan", 2);
+    }
+
+    @Test
+    public void passengerHasName(){
+        assertEquals("Eugene", passenger1.getName());
+    }
 }
