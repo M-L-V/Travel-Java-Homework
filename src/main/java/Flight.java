a84260ff986dcb7ad880ee8f4e2d1cd7f73847a0
Morgaine <morgainedv@gmail.com>
2020-01-10 19:03:47 +0000

    adds flight constructor

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String airport;
+    private String departureTime;
+
+    public Flight (){
+        this.passengers = new ArrayList<Passenger>();
+        this.plane = new Plane(PlaneType.GHOPPER);
+        this.flightNumber = "FR756";
+        this.destination = "LON";
+        this.airport = "EDINBURGH";
+        this.departureTime = "0900";
+
+
+
+
+
+    }
+
 }
