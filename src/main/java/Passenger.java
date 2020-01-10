5961151018ed1f1425527953add704565198fadd
Morgaine <morgainedv@gmail.com>
2020-01-10 18:14:18 +0000

    adds passenger class set up

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,10 @@
 public class Passenger {
+
+    private String name;
+    private int numOfBags;
+
+    public Passenger(String name, igaant numOfBags){
+        this.name = name;
+        this.numOfBags = numOfBags
+    }
 }
